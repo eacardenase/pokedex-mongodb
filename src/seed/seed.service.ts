30243d9d823137c4a5540cc6957e8114c32563f6
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-02 17:08:52 -0500

    feat: storing in database in batches

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
 
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { async } from 'rxjs';
 
 @Injectable()
 export class SeedService {
   private readonly axios: AxiosInstance = axios;
 
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
   async executeSeed() {
+    await this.pokemonModel.deleteMany({}); // erase db
+
     const { data } = await this.axios.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=10',
     );
 
-    data.results.forEach((pokemon) => {
+    const insertPromisesArray = [];
+
+    data.results.forEach(async (pokemon) => {
       const segments = pokemon.url.split('/');
       const no = +segments[segments.length - 2];
 
-      console.log(no);
+      insertPromisesArray.push(
+        this.pokemonModel.create({ no: no, name: pokemon.name }),
+      );
     });
 
-    return data.results;
+    await Promise.all(insertPromisesArray);
+
+    return 'Seed executed';
   }
 }
