0be4af442fd41d5e910b9fc3f4091369ee0ff2be
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-04 19:32:40 -0500

    feat: add schema validation with joi to env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,14 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { EnvConfiguration } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [EnvConfiguration],
-    }), // setup env variables
+      load: [EnvConfiguration], // setup env variables
+      validationSchema: JoiValidationSchema, // validates env variables and runs before EnvConfiguration
+    }),
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
